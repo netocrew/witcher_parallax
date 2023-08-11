eb03a0f192a74901dc2ae211a4df3777cbb62298
netocrew <adoter228@mail.ru>
2023-08-12 02:53:49 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider',{
+    // direction: 'vertical',
+    // freeMode: true,
+    speed: 2400,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 2.4
+    },
+    spaceBetween: 18,
+    parallax: true,
+})
